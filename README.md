c25939a7f774e5ab5e0289de042c4b4c6e78c417
Ubuntu <ubuntu@rust-test.lxd>
2023-01-27 13:54:34 +0000

    switched to dedicated debug host, and moved some code over from prettytable

--- README.md
+++ README.md
@@ -120,3 +120,61 @@ warning: `test-table` (bin "test-table") generated 1 warning
 $ 
 
 
+
+===== switched over to a dedicated debug host - an lxd container with a fresh ubuntu 20.04 install,
+with rust installed and "apt-get install build-essential" done, for cc.
+
+Starting the executable under gdb, I can see the "rows" seems completely out of whack:
+
+ubuntu@rust-test:~/test-rustc-1.65-1.67-segv$ gdb ./target/debug/test-table 
+GNU gdb (Ubuntu 9.2-0ubuntu1~20.04.1) 9.2
+Copyright (C) 2020 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+Type "show copying" and "show warranty" for details.
+This GDB was configured as "x86_64-linux-gnu".
+Type "show configuration" for configuration details.
+For bug reporting instructions, please see:
+<http://www.gnu.org/software/gdb/bugs/>.
+Find the GDB manual and other documentation resources online at:
+    <http://www.gnu.org/software/gdb/documentation/>.
+
+For help, type "help".
+Type "apropos word" to search for commands related to "word"...
+Reading symbols from ./target/debug/test-table...
+warning: Missing auto-load script at offset 0 in section .debug_gdb_scripts
+of file /home/ubuntu/test-rustc-1.65-1.67-segv/target/debug/test-table.
+Use `info auto-load python-scripts [REGEXP]' to list them.
+(gdb) b prettytable::TableSlice::get_all_column_width
+Breakpoint 1 at 0x14ca6: file src/lib.rs, line 119.
+(gdb) r
+Starting program: /home/ubuntu/test-rustc-1.65-1.67-segv/target/debug/test-table 
+[Thread debugging using libthread_db enabled]
+Using host libthread_db library "/lib/x86_64-linux-gnu/libthread_db.so.1".
+
+Breakpoint 1, prettytable::TableSlice::get_all_column_width (self=0x7fffffffe260) at src/lib.rs:119
+119	        let colnum = self.get_column_num();
+(gdb) p self
+$1 = (*mut prettytable::TableSlice) 0x7fffffffe260
+(gdb) p *self
+$2 = prettytable::TableSlice {format: 0x0, titles: 0x8, rows: &[prettytable::row::Row] {data_ptr: 0x555555619ba0, length: 93824993041104}}
+(gdb) s
+prettytable::TableSlice::get_column_num (self=0x7fffffffe260) at src/lib.rs:75
+75	        let mut cnum = 0;
+(gdb) 
+76	        for r in self.rows {
+(gdb) 
+core::slice::iter::<impl core::iter::traits::collect::IntoIterator for &[T]>::into_iter (self=...)
+    at /rustc/fc594f15669680fa70d255faec3ca3fb507c3405/library/core/src/slice/iter.rs:24
+24	/rustc/fc594f15669680fa70d255faec3ca3fb507c3405/library/core/src/slice/iter.rs: No such file or directory.
+(gdb) n
+25	in /rustc/fc594f15669680fa70d255faec3ca3fb507c3405/library/core/src/slice/iter.rs
+(gdb) n
+prettytable::TableSlice::get_column_num (self=0x7fffffffe260) at src/lib.rs:77
+77	            let l = r.len();
+(gdb) p r
+$3 = (*mut prettytable::row::Row) 0x555555619ba0
+(gdb) p r.len()
+$4 = 184683593789
+(gdb) n
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,113 @@
 #[macro_use]
 extern crate prettytable;
-use prettytable::Table;
+use prettytable::format::TableFormat;
+use prettytable::row::Row;
+
+use std::io::{self, Error, Write};
+use std::mem::transmute;
+
+/// An owned printable table
+#[derive(Clone, Debug)]
+pub struct Table {
+    format: Box<TableFormat>,
+    titles: Box<Option<Row>>,
+    rows: Vec<Row>,
+}
+
+#[derive(Clone, Debug)]
+pub struct TableSlice<'a> {
+    format: &'a TableFormat,
+    titles: &'a Option<Row>,
+    rows: &'a [Row],
+}
+
+impl<'a> TableSlice<'a> {
+    /// Compute and return the number of column
+    pub fn get_column_num(&self) -> usize {
+        let mut cnum = 0;
+        for r in self.rows {
+            let l = r.len();
+            if l > cnum {
+                cnum = l;
+            }
+        }
+        cnum
+    }
+
+    /// Get the number of rows
+    pub fn len(&self) -> usize {
+        self.rows.len()
+    }
+
+    fn get_column_width(&self, col_idx: usize) -> usize {
+        let mut width = match *self.titles {
+            Some(ref t) => t.get_cell_width(col_idx),
+            None => 0,
+        };
+        for r in self.rows {
+            let l = r.get_cell_width(col_idx);
+            if l > width {
+                width = l;
+            }
+        }
+        width
+    }
+
+    fn get_all_column_width(&self) -> Vec<usize> {
+        let colnum = self.get_column_num();
+        let mut col_width = vec![0usize; colnum];
+        for i in 0..colnum {
+            col_width[i] = self.get_column_width(i);
+        }
+        col_width
+    }
+
+    pub fn printstd(&self) {
+        let col_width = self.get_all_column_width();
+    }
+}
+
+impl<'a> AsRef<TableSlice<'a>> for TableSlice<'a> {
+    fn as_ref(&self) -> &TableSlice<'a> {
+        self
+    }
+}
+
+impl<'a> AsRef<TableSlice<'a>> for Table {
+    fn as_ref(&self) -> &TableSlice<'a> {
+        unsafe {
+            // All this is a bit hacky. Let's try to find something else
+            let s = &mut *((self as *const Table) as *mut Table);
+            s.rows.shrink_to_fit();
+            transmute(self)
+        }
+    }
+}
+
+impl Table {
+    /// Create an empty table
+    pub fn new() -> Table {
+        Self::init(Vec::new())
+    }
+
+    /// Create a table initialized with `rows`
+    pub fn init(rows: Vec<Row>) -> Table {
+        Table {
+            rows: rows,
+            titles: Box::new(None),
+            format: Box::new(*prettytable::format::consts::FORMAT_DEFAULT),
+        }
+    }
+}
+
+fn check_result<'a>(t1: &Table, t2: &TableSlice<'a>) {
+    println!("debug here");
+}
 
 fn main() {
-    use prettytable::format;
-
     let mut table = Table::new();
 
-/*
+    /*
     table.set_titles(row![
         "ServiceID",
         "ServiceName",
@@ -27,5 +127,9 @@ fn main() {
         ""
     ]);
     */
-    table.printstd();
+
+    let table_ref = table.as_ref();
+    check_result(&table, table_ref);
+
+    table_ref.printstd();
 }
